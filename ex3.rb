454acf744cdbc23eb770ae690533cbfe1aeb173a
AlbanD <exxaxion@gmail.com>
2016-07-11 17:39:26 +0200

    Fini les exercices 1 à 3

---
+++ ex3.rb
@@ -0,0 +1,36 @@
+# affiche le texte suivant :
+puts "I will now count my chickens:"
+
+# calcule pour Hens
+puts "Hens #{25 + 30 / 6}"
+# calcule pour Roosters 100-(25x3)x4/100, ajout de float à la place des %
+puts "Roosters #{100 - 25 * 3 * 0.4}"
+
+# affiche le texte suivant :
+puts "Now I will count the eggs:"
+
+# maths ajout de float à la place des %, resultat oeuf = 7.8
+puts 3 + 2 + 1 - 5 + 4 * 0.2 - 1 / 4 + 6
+
+# affiche le texte suivant :
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# affiche le resultat si vrai ou faux
+puts 3 + 2 < 5 - 7
+
+# maths
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+# affiche le texte
+puts "Oh, that's why it's false."
+
+# affiche le texte
+puts "How about some more."
+
+# affiche si plus grand que
+puts "Is it greater? #{5 > -2}"
+# affiche si c'est plus grand ou egal que
+puts "Is it greater or equal? #{5 >= -2}"
+# affiche si c'est plus petit ou egal que
+puts "Is it less or equal? #{5 <= -2}"
